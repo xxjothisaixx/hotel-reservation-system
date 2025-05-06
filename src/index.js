673ae136674bbc5399c23e11bc94e829a215c381
xxjothisaixx <167108073+xxjothisaixx@users.noreply.github.com>
2025-05-06 10:25:00 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import app from './app.js';
+import connectDB from './db/connectDB.js';
+import customError from './utils/customErrorHandler.js';
+import globalErrorHandler from './middlewares/globalErrorHandler.middleware.js';
+
+// error handling for unhandled routes....
+app.all('*', (req, _, next) => {
+    return next(new customError(404, `can't find ${req.originalUrl} on the server`));
+});
+
+// global error handler middleware...
+app.use(globalErrorHandler);
+
+(async () => {
+    try {
+        await connectDB();
+
+        // server running...
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(` Server listening on PORT ${process.env.PORT || 8000}`);
+        });
+    } catch (error) {
+        console.log(`Something error happened while connecting to server: \n ${error}`);
+    }
+})();
